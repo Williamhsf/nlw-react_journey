ca15405f80c0cacb137defc16b40882040a6c062
Williamhsf <williamhenriquefb@gmail.com>
2024-08-04 23:22:14 -0300

    aula 3

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -4,19 +4,23 @@ import { InviteGuestsModal } from './invite-guests-modal'
 import { ConfirmTripModal } from './confirm-trip-modal'
 import { DestinationAndDateStep } from './steps/destination-and-date-step'
 import { InviteGuestsStep } from './steps/invite-guests-step'
+import { DateRange } from 'react-day-picker'
+import { api } from '../../lib/axios'
 
 export function CreateTripPage() {
-  const navigate = useNavigate() 
-
+  const navigate = useNavigate()
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
   const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false)
-  
-  
   const [emailsToInvite, setEmailsToInvite] = useState([
-    'jessica.white44@yahoo.com',
-    'erik_leffler3@gmail.com',
+    'diego@rocketseat.com.br',
+    'john@acme.com'
   ])
+
+  const [destination, setDestination] = useState('')
+  const [ownerName, setOwnerName] = useState('')
+  const [ownerEmail, setOwnerEmail] = useState('')
+  const [eventStartAndEndDates, setEventStartAndEndDates] = useState<DateRange | undefined>()
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true)
@@ -48,7 +52,7 @@ export function CreateTripPage() {
     const data = new FormData(event.currentTarget)
     const email = data.get('email')?.toString()
 
-    if(!email) {
+    if (!email) {
       return
     }
 
@@ -70,56 +74,91 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList)
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>) {
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    navigate('/trips/123')
+
+    if (!destination) {
+      return
+    }
+
+    if (!eventStartAndEndDates?.from || !eventStartAndEndDates?.to) {
+      return
+    }
+
+    if (emailsToInvite.length === 0) {
+      return
+    }
+
+    if (!ownerName || !ownerEmail) {
+      return
+    }
+
+    const response = await api.post('/trips', {
+      destination,
+      starts_at: eventStartAndEndDates.from,
+      ends_at: eventStartAndEndDates.to,
+      emails_to_invite: emailsToInvite,
+      owner_name: ownerName,
+      owner_email: ownerEmail
+    })
+
+    const { tripId } = response.data
+
+    navigate(`/trips/${tripId}`)
   }
 
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
-
-        <div className='flex flex-col items-center gap-3'>
+        <div className="flex flex-col items-center gap-3">
           <img src="/logo.svg" alt="plann.er" />
-          <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+          <p className="text-zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
         </div>
-        
-        <div className='space-y-4'>
-          <DestinationAndDateStep
+
+        <div className="space-y-4">
+          <DestinationAndDateStep 
             closeGuestsInput={closeGuestsInput}
             isGuestsInputOpen={isGuestsInputOpen}
             openGuestsInput={openGuestsInput}
+            setDestination={setDestination}
+            setEventStartAndEndDates={setEventStartAndEndDates}
+            eventStartAndEndDates={eventStartAndEndDates}
           />
+
           {isGuestsInputOpen && (
-            <InviteGuestsStep
+            <InviteGuestsStep 
               emailsToInvite={emailsToInvite}
               openConfirmTripModal={openConfirmTripModal}
               openGuestsModal={openGuestsModal}
-            />            
+            />
           )}
         </div>
-        
-        <p className="text-zinc-500 text-sm">
+
+        <p className="text-sm text-zinc-500">
           Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
           com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
         </p>
-    </div>
+      </div>
 
-    {isGuestsModalOpen && (
-      <InviteGuestsModal  
-        emailsToInvite={emailsToInvite}
-        addNewEmailToInvite={addNewEmailToInvite}
-        closeGuestsModal={closeGuestsModal}
-        removeEmailFromInvite={removeEmailFromInvite}      
-      />
-    )}
+      {isGuestsModalOpen && (
+        <InviteGuestsModal 
+          emailsToInvite={emailsToInvite}
+          addNewEmailToInvite={addNewEmailToInvite}
+          closeGuestsModal={closeGuestsModal}
+          removeEmailFromInvite={removeEmailFromInvite}
+        />
+      )}
 
-    {isConfirmTripModalOpen && (
-      <ConfirmTripModal
-        closeConfirmTripModal={closeConfirmTripModal}
-        createTrip={createTrip}
-      />
-    )}
+      {isConfirmTripModalOpen && (
+        <ConfirmTripModal 
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+          setOwnerName={setOwnerName}
+          setOwnerEmail={setOwnerEmail}
+        />
+      )}
     </div>
-  )
+  );
 }
